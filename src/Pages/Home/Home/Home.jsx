5a8fcba518239fe14e67f1b909979168dbda85f2
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-11-16 00:28:32 +0600

    banner

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
     <div>
       <h1>THis is Home</h1>
+      <Banner></Banner>
     </div>
   );
 };
