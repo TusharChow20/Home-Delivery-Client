52c50da4e426523820ae77dfe3f4c3e664283c7e
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-11-28 21:13:46 +0600

    payment gateway

--- src/Pages/Dashboard/PaymentSuccess.jsx
+++ src/Pages/Dashboard/PaymentSuccess.jsx
@@ -1,19 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router";
 import useAxiosSecurity from "../../Hooks/useAxiosSecurity";
 
 const PaymentSuccess = () => {
   const [searchParams] = useSearchParams();
   const sessionId = searchParams.get("session_id");
+  const [paymentInfo, setPaymentInfo] = useState();
   const axiosSecure = useAxiosSecurity();
   useEffect(() => {
     if (sessionId) {
-      axiosSecure.patch(`payment-success?session_id=${sessionId}`);
+      axiosSecure
+        .patch(`payment-success?session_id=${sessionId}`)
+        .then((res) => {
+          setPaymentInfo({
+            transactionId: res.data.transactionId,
+            trackingId: res.data.trackingId,
+          });
+        });
     }
   }, [sessionId, axiosSecure]);
   return (
     <div>
       <h1>Payment success</h1>
+      <p>Transaction Id: {paymentInfo?.transactionId}</p>
+      <p>Tracking Id: {paymentInfo?.trackingId}</p>
     </div>
   );
 };
