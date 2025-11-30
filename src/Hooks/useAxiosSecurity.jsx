fe606a12938635ddb9c2626cc26b80e52f2f090e
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-12-01 01:26:42 +0600

    token verification

--- src/Hooks/useAxiosSecurity.jsx
+++ src/Hooks/useAxiosSecurity.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:3000",
@@ -8,6 +10,35 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecurity = () => {
+  const { user, signOutUser } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const requestInterceptor = axiosSecure.interceptors.request.use(
+      (config) => {
+        config.headers.Authorization = `Bearer ${user.accessToken} `;
+
+        return config;
+      }
+    );
+    const responseInterceptor = axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (err) => {
+        // console.log(err);
+        if (err.status === 401 || err.status === 403) {
+          signOutUser().then(() => {
+            navigate("/login");
+          });
+        }
+        return Promise.reject(err);
+      }
+    );
+    return () => {
+      axiosSecure.interceptors.request.eject(requestInterceptor);
+      axiosSecure.interceptors.response.eject(responseInterceptor);
+    };
+  }, [user]);
   return axiosSecure;
 };
 
